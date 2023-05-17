b5c3deb30552dfb0d777958a765979f841a6b740
Danny Steep Vera Palomino <danny.verap@gmail.com>
2023-05-17 00:00:04 -0500

    hola

--- Client/src/App.js
+++ Client/src/App.js
@@ -32,7 +32,7 @@ function App() {
    },[access])
 
    const onSearch = (id) => {
-      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      axios(`//localhost:3001/rickandmorty/character/${id}`)
          .then(({ data }) => {
             if (data.name) {
                setCharacters((oldChars) => [...oldChars, data]);
