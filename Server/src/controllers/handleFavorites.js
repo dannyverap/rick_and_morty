eaa157cfe229b5ed91bcd04dafbb5d1e700f5c52
danny <danny.verap@gmail.com>
2023-05-21 22:23:52 -0500

    hola

---
+++ Server/src/controllers/handleFavorites.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+let myFavorites = [];
+
+const postFav = (req, res) => {
+    const character = req.body;
+    myFavorites.push(character)
+    res.json(myFavorites)
+}
+
+const deleteFav = (req, res) => {
+    const { id } = req.params;
+
+    myFavorites = myFavorites.filter((favorite) => favorite.id !== +id) 
+
+    res.json(myFavorites)
+}
+
+module.exports = {
+    postFav,
+    deleteFav
+}
