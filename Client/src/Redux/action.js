64fd0d86cd905bc0590d1f6d3886e745384177fd
Danny Steep Vera Palomino <danny.verap@gmail.com>
2023-05-23 07:30:03 -0500

    asynAwait

--- Client/src/Redux/action.js
+++ Client/src/Redux/action.js
@@ -5,14 +5,18 @@ import axios from "axios";
 // };
 export const addFav = (character) => {
     const endpoint = 'http://localhost:3001/rickandmorty/fav';
-    return (dispatch) => {
-        axios.post(endpoint, character).then(({ data }) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.post(endpoint, character)
             return dispatch({
                 type: 'ADD_FAV',
                 payload: data,
-            });
-        });
-    };
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 };
 
 
@@ -20,17 +24,19 @@ export const addFav = (character) => {
 // export const removeFav = (id) => {
 //     return { type: "REMOVE_FAV", payload: id }
 // };
-    
+
 export const removeFav = (id) => {
     const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
-    return (dispatch) => {
-        axios.delete(endpoint)
-            .then(({ data }) => {
-                return dispatch({
-                    type: 'REMOVE_FAV',
-                    payload: data,
-                });
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.delete(endpoint)
+            return dispatch({
+                type: 'REMOVE_FAV',
+                payload: data,
             });
+        } catch (error) {
+            console.log(error);
+        }
     };
 };
 
