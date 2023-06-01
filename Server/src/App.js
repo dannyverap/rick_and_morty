8dd82d7c8c0901c0bf0bcb617e1d15f18a7df3b6
Danny Steep Vera Palomino <danny.verap@gmail.com>
2023-06-01 08:59:23 -0500

    sequelize

---
+++ Server/src/App.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const server = express();
+const router = require("./routes/index");
+const morgan = require("morgan");
+
+
+//! Middlewares
+server.use(express.json());
+server.use(morgan("dev"));
+
+//! CORS headers
+server.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+   );
+   res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, DELETE'
+   );
+   next();
+});
+
+//! Rutas
+server.use("/rickandmorty/", router); // cuando te hagan una petición a rickandmorty, buscará en el router
+
+module.exports = server;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,28 +1,8 @@
-const express = require("express");
-const server = express();
+const { conn } = require('./DB_connection');
+const server = require('./App');
 const PORT = 3001;
-const router = require("./routes/index");
-const morgan = require("morgan")
 
-
-server.use(express.json());
-server.use(morgan("dev")) // sirve para ver en consola las request
-
-
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-   );
-   res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-   );
-   next();
-});
-
-server.use("/rickandmorty/", router); //cuando te hagan una petición a rickandmorty andas a buscar al router
-
-server.listen(PORT, () => console.log(`server raised in port: ${PORT}`))
+//! Sync database and start server
+conn.sync({ force: true }).then(() =>
+   server.listen(PORT, () => console.log(`server raised in port: ${PORT}`))
+);
